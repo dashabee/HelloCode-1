6f2fa8f033f6267da2706edf34adeac9e7cf2dba
dashabee <dashabee>
2021-12-21 22:26:21 +0300

    task002

---
+++ Task002/Program.cs
@@ -0,0 +1,32 @@
+﻿// По заданному номеру дня недели вывести его название
+int WeekNumber = 7;
+
+if (WeekNumber == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (WeekNumber == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (WeekNumber == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (WeekNumber == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (WeekNumber == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (WeekNumber == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (WeekNumber == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
