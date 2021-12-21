770ed6c085e23b0a56850589ad1214be876e24e2
dashabee <dashabee>
2021-12-21 20:56:04 +0300

    task001

---
+++ Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+int numberA = 6;
+int numberB = 2;
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine ("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
