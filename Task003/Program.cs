8ae2ab016de65a695e6f5501d91011a2f4562c35
dashabee <dashabee>
2021-12-21 22:05:20 +0300

    task003

---
+++ Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int numberA = 1;
+int numberB = 2;
+if (numberA > numberB)
+{
+    Console.Write("Самое большое число ");
+    Console.Write(numberA);
+    Console.WriteLine("Самое маленькое число ");
+    Console.Write(numberB);
+}
+else
+{
+   Console.WriteLine("Самое большое число ");
+    Console.WriteLine(numberB);
+    Console.WriteLine("Cамое маленькое число ");
+    Console.WriteLine(numberA); 
+}
