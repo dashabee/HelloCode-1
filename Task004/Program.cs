607462948e97e7d3a58f7fe751874e35c7234084
dashabee <dashabee>
2021-12-21 23:09:25 +0300

    task004

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти максимальное из трех чисел
+int a = 12;
+int b = 8;
+int c = 10;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
